5e578c97f81d401b90d584b7c8fc3f5392d6877f
Rahinur Bin Naushad <rahinur2004@gmail.com>
2024-10-09 01:38:53 +0600

    nothing much

--- Lab 6/Task1/220041118_Lab06_task1.cpp
+++ Lab 6/Task1/220041118_Lab06_task1.cpp
@@ -6,17 +6,16 @@ using namespace std;
 
 int main()
 {
-    Counter c1(7,-2),c2,c3;
-    cout << c1.getCount()<<endl;
+    Counter c1,c2(8),c3(10),c4;
+    // cout << c1.getCount()<<endl;
 
-    c1++;
-    cout << c1.getCount() << endl;
+    c1=c2.operator+(c3);
+    c4=c2+c3;
+    cout << c1.getCount()<<' '<< c4.getCount() << endl;
 
-    c2+=c1;
+
+    // c2+=c1;
     // cout << c1.getCount() << endl;
-    cout << c2.getCount() << endl;
-
-    
-
+    // cout << c2.getCount() << endl;
     return 0;
 }
